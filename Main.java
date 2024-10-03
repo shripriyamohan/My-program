f1830ef86a634230f55d87eda73cabbfcf59f102
shripriyamohan <shripriyamohan@gmail.com>
2024-10-03 15:09:53 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[])
+    {
+      Scanner a=new Scanner(System.in);
+      int b=a.nextInt();
+      int c=a.nextInt();
+      System.out.println(b+c);
+    }
+    
+    
+}
